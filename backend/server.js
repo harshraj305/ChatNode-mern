3f9a3a85539221f43cd8ad922b635159e2dda859
harshraj305 <1700harshraj@gmail.com>
2023-10-13 13:42:03 +0530

    cors changed to deployment link

--- backend/server.js
+++ backend/server.js
@@ -57,7 +57,8 @@ const server = app.listen(PORT, () => {
 const io = require('socket.io')(server, {
     pingTimeout: 60000,
     cors: {
-        origin: 'http://localhost:3000',
+        // origin: 'http://localhost:3000',
+        origin: 'https://chatnode-hp6n.onrender.com/chats',
     }
 });
 
